6201e88316dd1964e56165213f74439e76b96c23
Sander Muniz <cayosandermuniz@gmail.com>
2025-06-11 09:46:30 -0300

    comentando opcoes de erros

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // Inicializa o Firebase (garante que seja feito uma única vez)
-firebaseService.initializeFirebase();
+//firebaseService.initializeFirebase();
 
 app.post('/webhook/kiwify', async (req, res) => {
   const payload = req.body;
@@ -19,19 +19,21 @@ app.post('/webhook/kiwify', async (req, res) => {
   }
 
   // Processa o payload para incluir apenas os campos desejados
-  const dataToSave = processKiwifyPayload(payload);
+//  const dataToSave = processKiwifyPayload(payload);
 
   // Use um caminho no Firebase que faça sentido para você.
   // Por exemplo, você pode usar o order_id como parte do caminho ou apenas 'kiwify_orders'.
-  const firebasePath = `kiwify_webhooks/${payload.webhook_event_type || 'unclassified'}`;
+//  const firebasePath = `kiwify_webhooks/${payload.webhook_event_type || 'unclassified'}`;
 
-  const success = await firebaseService.savePayload(firebasePath, dataToSave);
+//  const success = await firebaseService.savePayload(firebasePath, dataToSave);
 
+/*
   if (success) {
     res.status(200).send('Payload recebido e salvo com sucesso no Firebase!');
   } else {
     res.status(500).send('Erro ao processar e salvar o payload.');
   }
+  */
 });
 
 // Endpoint para verificar se o webhook está funcionando (opcional)
